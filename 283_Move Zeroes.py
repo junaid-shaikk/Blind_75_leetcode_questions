e851c2670f1221628ff60b697e78c1114b996a7e
Junaid Shaik <70793625+junaid-shaikk@users.noreply.github.com>
2022-06-19 01:21:29 +0530

    Create 283_Move Zeroes.py

---
+++ 283_Move Zeroes.py
@@ -0,0 +1,8 @@
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i==0:
+                nums.pop(nums.index(0))
+                nums.append(0)
