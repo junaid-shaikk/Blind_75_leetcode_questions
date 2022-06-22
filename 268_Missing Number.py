59624d5bdfcfa0e9eea5f338e629acb363a6dc39
Junaid Shaik <70793625+junaid-shaikk@users.noreply.github.com>
2022-06-23 01:14:05 +0530

    Update 268_Missing Number.py

--- 268_Missing Number.py
+++ 268_Missing Number.py
@@ -19,7 +19,6 @@ Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        n=len(nums)
+        return (n*(n+1)//2)-(sum(nums))
         
